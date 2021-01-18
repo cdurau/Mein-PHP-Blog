f8d54f43b780641bcd3565cdfb0bf616164dd77c
cdurau <{ID}+{username}@users.noreply.github.com>
2021-01-18 19:44:25 +0100

    Get Posts from DB

--- database.php
+++ database.php
@@ -2,3 +2,13 @@
 
 $pdo = new PDO('mysql:host=192.168.2.30;port=3306;dbname=blog', 'blog', '2JH0chvcVuBj77v4');
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+class Database
+{
+    public static function fetchPosts()
+    {
+        global $pdo;
+
+        return $pdo->query("SELECT * FROM `posts`");
+    }
+}
--- pages/index.php
+++ pages/index.php
@@ -4,5 +4,14 @@ include "elements/header.php";
 ?>
 
     <h1>Startseite des Blogs</h1>
+    <p class="lead">Das hier ist die Startseite des Blogs</p>
+
+<?php
+$res = DATABASE::fetchPosts();
+
+foreach ($res as $row) {
+    echo $row["title"] . "<br>";
+}
+?>
 
 <?php include "elements/footer.php"; ?>
